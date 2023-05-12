cc2c975c66fac69bc2c97334123811cc1c1ebe70
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-05-12 12:22:50 +0530

    Add files via upload

---
+++ SQUATS.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for ( int i=0;i<t;i++)
+    {
+        int x;
+        scanf("%d",&x);
+        printf("%d\n",x*15);
+    }
+    return 0;
+}
+
