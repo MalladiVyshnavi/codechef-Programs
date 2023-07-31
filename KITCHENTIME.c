68e8222fe9db3f15e0ede60172e6f222828ecf1a
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-07-31 14:29:10 +0530

    Add files via upload

---
+++ KITCHENTIME.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int t,i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        int x,y;
+        scanf("%d%d",&x,&y);
+        printf("%d\n",y-x);
+    }
+}
