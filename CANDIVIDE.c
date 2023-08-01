2e98fa9d247fb9f39d921c627856bc416d478ba1
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-08-01 19:31:14 +0530

    Add files via upload

---
+++ CANDIVIDE.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int t,i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        int  n;
+        scanf("%d",&n);
+        if(n%3==0)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+}
