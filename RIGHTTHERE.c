4f61d73ee0c869b4786939dde6865ff3153aed77
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-08-02 13:53:17 +0530

    Add files via upload

---
+++ RIGHTTHERE.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int t,i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        int n,x;
+        scanf("%d%d",&n,&x);
+        if(n<=x)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+}
