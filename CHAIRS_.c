7701e7b71c794ddc32f4900dac8f0b0e7906cbce
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-08-02 14:38:55 +0530

    Add files via upload

--- CHAIRS_.c
+++ CHAIRS_.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int main()
 {
     int t,i;
@@ -7,7 +6,7 @@ int main()
     for(i=0;i<t;i++)
     {
         int x,y;
-        scanf("%d %d",&x,&y);
+        scanf("%d%d",&x,&y);
         if(x>y)
         {
             printf("%d\n",x-y);
