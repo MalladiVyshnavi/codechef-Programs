01a7cf7883d75e57e7f0e5d79a222c0722ad859f
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-08-12 09:44:38 +0530

    Add files via upload

---
+++ FBC.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int t,i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        int k,x;
+        scanf("%d%d",&k,&x);
+        printf("%d\n",k-x);
+        
+    }
+}
