f1850693b1ea4c0aa253eb771469ddcc001c20ad
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-08-21 19:18:12 +0530

    Add files via upload

---
+++ F1RULE.c
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+ int main()
+ {
+     int t,i;
+     scanf("%d",&t);
+     for(i=0;i<t;i++)
+     {
+         int x,y;
+	    scanf("%d%d",&x,&y);
+	    if(y<=1.07*x)
+	    {
+	        printf("yes\n");
+	        
+	    }
+	    else{
+	        printf("no\n");
+	        
+	    }
+     }
+ }
