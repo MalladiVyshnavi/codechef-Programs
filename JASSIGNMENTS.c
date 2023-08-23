0b00ac7bef8e017614ce6873bc6a33e454a8151c
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-08-23 19:22:33 +0530

    Add files via upload

---
+++ JASSIGNMENTS.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int t,x;
+	scanf("%d",&t);
+	while(t--){
+	    scanf("%d",&x);
+	    if(x+2<=9)
+	    printf("yes\n");
+	    else
+	    printf("no\n");
+	}
+}
+
