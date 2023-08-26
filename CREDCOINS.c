38dd99049502a793d262fd86aa3c32bf1b6443b6
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-08-26 11:21:41 +0530

    Add files via upload

---
+++ CREDCOINS.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a;
+	scanf("%d",&a);
+	while(a--){
+	    int b,c;
+	    scanf("%d %d",&b ,&c);
+	    printf("%d\n",b*c/100);
+	}
+	return 0;
+}
