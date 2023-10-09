110634f9acbfc125b740e2b8f624a56a61d5c42e
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-10-09 13:31:51 +0530

    Add files via upload

---
+++ FLOORS.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		   int p = (((x-1)/10)+1);
+	       int q = (((y-1)/10)+1);
+	       System.out.println(Math.abs(p-q));  
+		    
+		}
+	}
+}
