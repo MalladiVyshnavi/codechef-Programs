4c1c873f68dae61508d5ae8dfdcbd47617ec51f3
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-10-15 11:40:07 +0530

    Add files via upload

---
+++ BUYLAMP.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		while(t>0){
+		    int n,k,x,y;
+		    n=s.nextInt();
+		    k=s.nextInt();
+		    x=s.nextInt();
+		    y=s.nextInt();
+		    if(x>y)
+		        System.out.println(k*x+(n-k)*y);
+		    else
+		        System.out.println(n*x);
+		        t--;
+		 }
+	}
+	
+}
