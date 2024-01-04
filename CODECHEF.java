54ea85404b461df103a8ad42f228c086530d6b52
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2024-01-04 21:37:30 +0530

    Add files via upload

---
+++ CODECHEF.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n==1 || n==2 || n==3 || n==5 || n==6 || n==7)
+		{
+		    System.out.println("NO");
+		}
+		else
+		{
+		    System.out.println("YES");
+		}
+
+	}
+}
