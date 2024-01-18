5aceabe114a1663e1e95020287933b4071453a80
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2024-01-18 11:18:16 +0530

    Add files via upload

---
+++ PHONEYR.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int currentyear=sc.nextInt();
+		int last=currentyear%100;
+		System.out.printf("k%02d\n",last);
+		
+
+	}
+}
