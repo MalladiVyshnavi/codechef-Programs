2231b4b16a401a4168d7f8db59f5faacb0d83166
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2024-01-21 18:08:03 +0530

    Add files via upload

---
+++ SPCP1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int w=sc.nextInt();
+	    int h=sc.nextInt();
+	    if(w>=60 && h<=130)
+	    {
+	        System.out.println("YES");
+	    }
+	    else
+	    {
+	        System.out.println("NO");
+	    }
+
+
+	}
+}
