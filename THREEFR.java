6646adc176959416654ace54d5a2f5f92833504d
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2024-02-12 15:04:25 +0530

    Add files via upload

---
+++ THREEFR.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    int z=sc.nextInt();
+		    if(x+y==z || y+z==x || z+x==y)
+		    {
+		        System.out.println("yes");
+		    }
+		    else
+		    {
+		        System.out.println("no");
+		    }
+		}
+
+	}
+}
