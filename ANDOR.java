c79f95709af15e347b9fc031d43eeb0ef302d029
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2024-02-16 19:47:20 +0530

    Add files via upload

---
+++ ANDOR.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    long n = sc.nextLong();
+	        if(n == 0)
+	        {
+	            System.out.println(-1);
+	        }
+	        else
+	        {
+	            System.out.println(0+" "+n);
+	        }
+		    
+		}
+
+	}
+}
