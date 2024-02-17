0d953b2f2b1506cb6c8f3a00423a8e7e84c3671c
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2024-02-17 11:36:35 +0530

    Add files via upload

---
+++ APPLEORANGE.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	 public static int gcd (int a , int b){
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t!=0){
+		    int n = s.nextInt();
+		    int m = s.nextInt();
+		    int val = gcd(n,m);
+		    System.out.println(val);
+		    
+		    t--;
+		}
+	}
+}
