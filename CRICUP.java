a7e54b92fb59dff37a4cae6558dde1c87e638fb3
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2024-02-21 14:09:14 +0530

    Add files via upload

---
+++ CRICUP.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    int d=sc.nextInt();
+		    int a=x-y;
+		    if(a<=d)
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
