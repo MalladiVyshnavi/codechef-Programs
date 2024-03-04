0eafe2183cd1e44bb931aa7a1bca9a2a2f3222ab
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2024-03-04 14:08:51 +0530

    Add files via upload

---
+++ AVGOF3.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int x = sc.nextInt();
+            int a = 3*x;
+            int a1=1;
+            int a2= 3;
+            int a3= a-a1-a2;
+            System.out.println(a1);
+            System.out.println(a2);
+            System.out.println(a3);
+            
+
+
+        }
+	}
+}
